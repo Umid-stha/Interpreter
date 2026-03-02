d1cdddd25dfa5e4b91d41c905325284d01977ac3
Umid-stha <shresthaumidccc@gmail.com>
2026-03-02 08:51:17 +0000

    added documentation for functions

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -6,17 +6,19 @@ import (
 
 type Lexer struct {
 	input        string
-	position     int
-	readPosition int
+	position     int // current position in input (points to current char)
+	readPosition int // current reading position in input (after current char)
 	ch           byte
 }
 
+// initialize the lexer with the input and read the first character
 func New(input string) *Lexer {
 	lexer := &Lexer{input: input}
 	lexer.readChar()
 	return lexer
 }
 
+// readChar advances our position in the input string or sets the current character to 0 if we've reached the end of the input
 func (l *Lexer) readChar() {
 	if l.readPosition >= len(l.input) {
 		l.ch = 0
@@ -27,6 +29,11 @@ func (l *Lexer) readChar() {
 	l.readPosition++
 }
 
+/*
+nextToken checks the current character and returns the appropriate token based on it.
+It uses a switch statement to determine the type of token to return.
+After determining the token, it calls readChar to advance to the next character in the input.
+*/
 func (l *Lexer) nextToken() token.Token {
 	var tok token.Token
 	switch l.ch {
@@ -54,6 +61,7 @@ func (l *Lexer) nextToken() token.Token {
 	return tok
 }
 
+// newToken is a helper function that creates a new token based on the given token type and character.
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
 }
